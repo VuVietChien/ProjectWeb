eb71f7019fb69dae02c07012cf38268237051736
thuanbin1101 <thuanbin1108@gmail.com>
2021-11-07 21:54:03 +0700

    OKELA

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $baseUrl = '';
 include_once('./layouts/header.php');
 $db = new Database();
@@ -7,98 +7,89 @@ $lastestItems = $db->executeResult($sql);
 
 
 $sql1 = "select * from sliders";
-$slider = $db->executeResult($sql1);
+$sliders = $db->executeResult($sql1);
 ?>
 <div class="preloader loaded" id="preloader"><img src="./preload.gif" alt=""></div>
 <!-- banner -->
 
 <div id="demo" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ul class="carousel-indicators">
-    <li data-target="#demo" data-slide-to="0" class="active"></li>
-    <li data-target="#demo" data-slide-to="1"></li>
-    <li data-target="#demo" data-slide-to="2"></li>
-  </ul>
+	<!-- Indicators -->
+	<ul class="carousel-indicators">
+		<li data-target="#demo" data-slide-to="0" class="active"></li>
+		<li data-target="#demo" data-slide-to="1"></li>
+		<li data-target="#demo" data-slide-to="2"></li>
+	</ul>
 
-  <!-- The slideshow -->
-<?php
-  
-  foreach($slider as $item) {
+	<!-- The slideshow -->
+	<div class="carousel-inner">
+		<?php foreach ($sliders as $slider) : ?>
+			<?php if ($slider['id'] == 2) : ?>
+				<div class="carousel-item active">
+					<img src="<?= $slider['image'] ?>">
+				</div>
+			<?php else : ?>
+				<div class="carousel-item ">
+					<img src="<?= $slider['image'] ?>">
+				</div>
+			<?php endif ?>
+		<?php endforeach; ?>
+	</div>
 
-	if($item['id'] == 2) {
-		echo'<div class="carousel-inner">
-			<div class="carousel-item active">
-			<img src="'.$item['image'].'" alt="">
-			</div>
-		</div>';
-	}
-	else
-	{
-		echo'<div class="carousel-inner">
-			<div class="carousel-item">
-			<img src="'.$item['image'].'" alt="">
-			</div>
-		</div>';
-	}
-}
 
-?>
-  
-
-  <!-- Left and right controls -->
-  <a class="carousel-control-prev" href="#demo" data-slide="prev">
-    <span class="carousel-control-prev-icon"></span>
-  </a>
-  <a class="carousel-control-next" href="#demo" data-slide="next">
-    <span class="carousel-control-next-icon"></span>
-  </a>
+	<!-- Left and right controls -->
+	<a class="carousel-control-prev" href="#demo" data-slide="prev">
+		<span class="carousel-control-prev-icon"></span>
+	</a>
+	<a class="carousel-control-next" href="#demo" data-slide="next">
+		<span class="carousel-control-next-icon"></span>
+	</a>
 
 </div>
 <!-- banner stop -->
 <div class="container">
 	<h1 style="text-align: center; margin-top: 20px; margin-bottom: 20px;">SẢN PHẨM MỚI NHẤT</h1>
 	<div class="row">
-	<?php
-		foreach($lastestItems as $item) {
+		<?php
+		foreach ($lastestItems as $item) {
 			echo '<div class="col-md-3 col-6 product-item">
-					<a href="detail.php?id='.$item['id'].'"><img src="'.$item['image'].'" style="width: 100%; height: 220px;"></a>
-					<p style="font-weight: bold;">'.$item['category_name'].'</p>
-					<a href="detail.php?id='.$item['id'].'"><p style="font-weight: bold;">'.$item['name'].'</p></a>
-					<p style="color: red; font-weight: bold;">'.number_format($item['price']).' VND</p>
-					<p><button class="btn btn-success" onclick="addCart('.$item['id'].', 1)" style="width: 100%; border-radius: 0px;"><i class="bi bi-cart-plus-fill"></i> Thêm giỏ hàng</button></p>
+					<a href="detail.php?id=' . $item['id'] . '"><img src="' . $item['image'] . '" style="width: 100%; height: 220px;"></a>
+					<p style="font-weight: bold;">' . $item['category_name'] . '</p>
+					<a href="detail.php?id=' . $item['id'] . '"><p style="font-weight: bold;">' . $item['name'] . '</p></a>
+					<p style="color: red; font-weight: bold;">' . number_format($item['price']) . ' VND</p>
+					<p><button class="btn btn-success" onclick="addCart(' . $item['id'] . ', 1)" style="width: 100%; border-radius: 0px;"><i class="bi bi-cart-plus-fill"></i> Thêm giỏ hàng</button></p>
 				</div>';
 		}
-	?>
+		?>
 	</div>
 </div>
 <!-- danh muc san pham -->
 <?php
 $count = 0;
-foreach($menuItems as $item) {
-	$sql = "select products.*, categories.name as category_name from products left join categories on products.category_id = categories.id where products.category_id = ".$item['id'] . " AND deleted=0";
+foreach ($menuItems as $item) {
+	$sql = "select products.*, categories.name as category_name from products left join categories on products.category_id = categories.id where products.category_id = " . $item['id'] . " AND deleted=0";
 	// ." order by products.updated_at desc limit 0,4"
 	$items = $db->executeResult($sql);
-	if($items == null || count($items) < 4) continue;
+	if ($items == null || count($items) < 4) continue;
 
 ?>
-<div style="background-color: <?=($count++%2 == 0)?'#f7f9fa':''?>;">
-<div class="container">
-<h1 style="text-align: center; margin-top: 20px; margin-bottom: 20px;"><?=$item['name']?></h1>
-<div class="row">
-<?php
-	foreach($items as $pItem) {
-		echo '<div class="col-md-3 col-6 product-item">
-				<a href="detail.php?id='.$item['id'].'"><img src="'.$pItem['image'].'" style="width: 100%; height: 220px;"></a>
-				<p style="font-weight: bold;">'.$pItem['category_name'].'</p>
-				<a href="detail.php?id='.$item['id'].'"><p style="font-weight: bold;">'.$pItem['name'].'</p></a>
-				<p style="color: red; font-weight: bold;">'.number_format($pItem['price']).' VND</p>
-				<p><button class="btn btn-success" onclick="addCart('.$item['id'].', 1)" style="width: 100%; border-radius: 0px;"><i class="bi bi-cart-plus-fill"></i> Thêm giỏ hàng</button></p>
+	<div style="background-color: <?= ($count++ % 2 == 0) ? '#f7f9fa' : '' ?>;">
+		<div class="container">
+			<h1 style="text-align: center; margin-top: 20px; margin-bottom: 20px;"><?= $item['name'] ?></h1>
+			<div class="row">
+				<?php
+				foreach ($items as $pItem) {
+					echo '<div class="col-md-3 col-6 product-item">
+				<a href="detail.php?id=' . $item['id'] . '"><img src="' . $pItem['image'] . '" style="width: 100%; height: 220px;"></a>
+				<p style="font-weight: bold;">' . $pItem['category_name'] . '</p>
+				<a href="detail.php?id=' . $item['id'] . '"><p style="font-weight: bold;">' . $pItem['name'] . '</p></a>
+				<p style="color: red; font-weight: bold;">' . number_format($pItem['price']) . ' VND</p>
+				<p><button class="btn btn-success" onclick="addCart(' . $item['id'] . ', 1)" style="width: 100%; border-radius: 0px;"><i class="bi bi-cart-plus-fill"></i> Thêm giỏ hàng</button></p>
 			</div>';
-	}
-?>
-</div>
-</div>
-</div>
+				}
+				?>
+			</div>
+		</div>
+	</div>
 
 <?php
 }
